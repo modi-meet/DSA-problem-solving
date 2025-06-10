9a27d985fbf71ba91ca27edd8aadd4d402e5039d
Modi Meet <meetmodi8080@gmail.com>
2025-06-10 22:26:56 +0530

    modified rangeSum

--- binary-search-trees/rangeSum.cpp
+++ binary-search-trees/rangeSum.cpp
@@ -1,6 +1,5 @@
 // Given the root node of a binary search tree, 
 // Return the sum of values of all nodes with a value in the range [low, high].
-
 class Solution {
 public:    
     int rangeSumBST(TreeNode* root, int low, int high) {
@@ -35,3 +34,19 @@ public:
 };
 // TC: O(h) in (balanced BST), O(n) in worst case
 // SC: O(h)
+
+/*
+Explanation
+Two integer value low,high are given,
+return the sum of all the nodes between this range
+--
+Brute force will be traverse through all the nodes of the tree and add node->val to sum,
+if it falls b/w the interval.
+--
+Optimal Approach
+it will be,
+    To return sum from leftsubtree + rightsubtree
+    - if the current node->val is less than low, go to left subtree
+    - if greater, than go to right subtree
+Because solve for one node and rest will be taken care by recursion.
+*/
