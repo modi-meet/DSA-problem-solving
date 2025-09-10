65aeec050e4e086369c9a1222a407ce25a9ce00a
Modi Meet <meetmodi8080@gmail.com>
2025-09-10 11:50:03 +0530

    add tabulation

--- dynamic-programming/subsetPartition.java
+++ dynamic-programming/subsetPartition.java
@@ -39,3 +39,31 @@ class Solution {
 
 // Tabulation
 class Solution {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+
+        int totalSum = 0;
+        for (int num : nums) totalSum += num;
+
+        if (totalSum % 2 != 0) return false;
+        int target = totalSum / 2;
+        
+        boolean[][] dp = new boolean[n+1][target+1];
+        
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (nums[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j - nums[i-1]] || dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][target];
+    }
+};
