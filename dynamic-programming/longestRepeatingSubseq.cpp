e4a1fe22d49abf122befda092989df511b56c24e
Modi Meet <meetmodi8080@gmail.com>
2025-09-19 18:55:21 +0530

    Add repeatingSubseq

---
+++ dynamic-programming/longestRepeatingSubseq.cpp
@@ -0,0 +1,27 @@
+// Longest Repeating Subsequence
+// Given string str, find the length of the longest repeating subsequence
+
+class Solution {
+  public:
+    int LongestRepeatingSubsequence(string &s) {
+        int n = s.length();
+        string s1 = s, s2 = s;
+
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s1[i-1] == s2[j-1] && i != j) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[n][n];
+    }
+};
+// Time Complexity: O(n^2)
+// Space Complexity: O(n^2)
